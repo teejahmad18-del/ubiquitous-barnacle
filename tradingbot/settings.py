be4dc7219a7ce738c03001dc56dda8bb9d972d2a
teejahmad18-del <teejahmad18@gmail.com>
2026-05-09 22:34:10 +0100

    Deleted pycache and ignored garbage files

--- tradingbot/settings.py
+++ tradingbot/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'trading',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'   # Add this line
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
